cd5ec39bd7024d5a44a5ac8f26e77282c40c0517
leggat <leggat@cern.ch>
2016-07-19 00:15:24 +0800

    A utility to create the file lists for large datasets broken into smaller jobs

---
+++ utils/createFileLists.py
@@ -0,0 +1,40 @@
+#A short script that takes input from the user to create a set of file lists to run on condor. the inputs are:
+# Number of files per job - how many files to put in each file list
+# Name of dataset
+# Total number of files in dataset
+# Dataset's designated number according to the configuration file in configs
+
+import sys, math, mmap
+
+namesPerFile = float(raw_input("How many files to run over per job? "))
+if namesPerFile == 0:
+    print "You idiot, we can't divide by 0"
+    sys.exit()
+outputDirectory = raw_input("Output directory: ")
+
+while True:
+    datasetName = raw_input("Dataset name: ")
+    if datasetName == "end" or datasetName == "stop": break
+    datasetNum = raw_input("Corresponds to the number: ")
+    totalDatasetFiles = float(raw_input("Total number of files in this dataset: "))
+    nJobs = int(math.ceil(totalDatasetFiles/namesPerFile))
+    print "That means we will make " + str(nJobs) + " jobs"
+    remoteDir = raw_input("Remote directory: ")
+    bigDatasetFile = open("config/files/full/"+datasetName+".list")
+    s = mmap.mmap(bigDatasetFile.fileno(), 0, access=mmap.ACCESS_READ)
+    for i in range(nJobs):
+        currentFile = open(outputDirectory + datasetName + str(i) + ".list","w")
+        currentFile.write("Name: "+datasetName)
+        currentFile.write("\nNumber: " + datasetNum + "_1\n")
+        maxFiles = (i+1)*namesPerFile+1
+        if totalDatasetFiles+1 < maxFiles: maxFiles = totalDatasetFiles+1
+        for j in range(i*namesPerFile+1,maxFiles):
+            line = "OutTree_" + str(j) + ".root"
+            if s.find(line) != -1:
+                currentFile.write(remoteDir + line + "\n")
+                print line
+#            currentFile.write(remoteDir + "
+        
+        
+    
+print "Thank you for using the create jobs program. Have a nice day"
