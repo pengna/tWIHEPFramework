439fa3b520bdf9ed0b8910d639f10346773f0c40
leggat <leggat@cern.ch>
2016-09-15 23:18:13 +0800

    A script for creating condor submission directories/scripts for all regions for normal and inverted isolation

---
+++ utils/makeJobsForAllRegions.py
@@ -0,0 +1,11 @@
+#Make the job folders for all of the regions we want to study for the tW lepton+jets
+
+import subprocess
+
+baseDir = "/publicfs/cms/user/duncanleg/tW13TeV/framework/"
+
+for i in [""," data"]:
+    for k in [""," inv"]:
+        for j in ["", " ttbarReg", " wJetsReg"]:
+            print "maketWSubmit.py"+i+k+j
+            subprocess.call( "python "+baseDir+"utils/maketWSubmit.py"+i+k+j,shell=True)
