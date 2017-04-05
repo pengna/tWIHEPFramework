bb11cce53bd0c634bb2d9102734db9baf708690f
leggat <leggat@cern.ch>
2017-04-05 23:34:03 +0800

    I should unstage this because I change it for my own stuff and it's not useful to keep commiting these changes

--- utils/makeJobsForAllRegions.py
+++ utils/makeJobsForAllRegions.py
@@ -10,8 +10,9 @@ for i in [""," data"]:
 #    for k in [""]:
 #        for j in ["", " ttbarReg", " wJetsReg"," wJets2"]:
 #        for j in [" ttbarReg", " wJetsReg"]:
-        for j in [" ttbarReg", " wJets2"]:
+#        for j in [" ttbarReg", " wJets2"]:
 #        for j in [" wJets2"]:
-#        for j in [""]:
+#        for j in [" wJetsReg"]:
+        for j in [""]:
             print "maketWSubmit.py"+i+k+j
             subprocess.call( "python "+baseDir+"utils/maketWSubmit.py skims"+i+k+j,shell=True)
